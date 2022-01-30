3ade6630e1016745397962c504a762d925d72e9e
Ankeet Maini <ankeet.maini@hotstar.com>
2022-01-30 18:38:11 +0530

    Add tests

---
+++ __tests__/calver.test.js
@@ -0,0 +1,376 @@
+const core = require("@actions/core");
+const fs = require("fs");
+
+const calver = require("../index");
+
+jest.mock("@actions/core");
+jest.mock("fs");
+
+jest.useFakeTimers();
+jest.setSystemTime(1643530268736); // Jan 30, 2022​
+
+test("updates package json from non calver to calver", () => {
+  core.getInput.mockReturnValueOnce("abc");
+  core.getInput.mockReturnValueOnce("web");
+  fs.readFileSync.mockReturnValueOnce(
+    `
+{
+    "name": "calver",
+    "version": "1.0.0",
+    "main": "dist/index.js",
+    "author": "Ankeet Maini <ankeet.maini@gmail.com>",
+    "license": "MIT",
+    "scripts": {
+        "build": "ncc build index.js --license licenses.txt"
+    },
+    "dependencies": {
+        "@actions/core": "^1.6.0",
+        "@vercel/ncc": "^0.33.1"
+    },
+    "devDependencies": {
+        "jest": "^27.4.7"
+    }
+}
+`
+  );
+  calver();
+  expect(fs.writeFileSync).toHaveBeenCalledWith(
+    "abc",
+    `
+{
+    "name": "calver",
+    "version": "22.01.30.0",
+    "main": "dist/index.js",
+    "author": "Ankeet Maini <ankeet.maini@gmail.com>",
+    "license": "MIT",
+    "scripts": {
+        "build": "ncc build index.js --license licenses.txt"
+    },
+    "dependencies": {
+        "@actions/core": "^1.6.0",
+        "@vercel/ncc": "^0.33.1"
+    },
+    "devDependencies": {
+        "jest": "^27.4.7"
+    }
+}
+`
+  );
+});
+
+test("updates package json calver", () => {
+  core.getInput.mockReturnValueOnce("abc");
+  core.getInput.mockReturnValueOnce("web");
+  fs.readFileSync.mockReturnValueOnce(
+    `
+  {
+      "name": "calver",
+      "version": "22.01.30.2",
+      "main": "dist/index.js",
+      "author": "Ankeet Maini <ankeet.maini@gmail.com>",
+      "license": "MIT",
+      "scripts": {
+          "build": "ncc build index.js --license licenses.txt"
+      },
+      "dependencies": {
+          "@actions/core": "^1.6.0",
+          "@vercel/ncc": "^0.33.1"
+      },
+      "devDependencies": {
+          "jest": "^27.4.7"
+      }
+  }
+  `
+  );
+  calver();
+  expect(fs.writeFileSync).toHaveBeenCalledWith(
+    "abc",
+    `
+  {
+      "name": "calver",
+      "version": "22.01.30.3",
+      "main": "dist/index.js",
+      "author": "Ankeet Maini <ankeet.maini@gmail.com>",
+      "license": "MIT",
+      "scripts": {
+          "build": "ncc build index.js --license licenses.txt"
+      },
+      "dependencies": {
+          "@actions/core": "^1.6.0",
+          "@vercel/ncc": "^0.33.1"
+      },
+      "devDependencies": {
+          "jest": "^27.4.7"
+      }
+  }
+  `
+  );
+});
+
+test("updates package json calver - same day large number of deploys", () => {
+  core.getInput.mockReturnValueOnce("abc");
+  core.getInput.mockReturnValueOnce("web");
+  fs.readFileSync.mockReturnValueOnce(
+    `
+    {
+        "name": "calver",
+        "version": "22.01.30.2001",
+        "main": "dist/index.js",
+        "author": "Ankeet Maini <ankeet.maini@gmail.com>",
+        "license": "MIT",
+        "scripts": {
+            "build": "ncc build index.js --license licenses.txt"
+        },
+        "dependencies": {
+            "@actions/core": "^1.6.0",
+            "@vercel/ncc": "^0.33.1"
+        },
+        "devDependencies": {
+            "jest": "^27.4.7"
+        }
+    }
+    `
+  );
+  calver();
+  expect(fs.writeFileSync).toHaveBeenCalledWith(
+    "abc",
+    `
+    {
+        "name": "calver",
+        "version": "22.01.30.2002",
+        "main": "dist/index.js",
+        "author": "Ankeet Maini <ankeet.maini@gmail.com>",
+        "license": "MIT",
+        "scripts": {
+            "build": "ncc build index.js --license licenses.txt"
+        },
+        "dependencies": {
+            "@actions/core": "^1.6.0",
+            "@vercel/ncc": "^0.33.1"
+        },
+        "devDependencies": {
+            "jest": "^27.4.7"
+        }
+    }
+    `
+  );
+});
+
+test("updates package json calver different dates", () => {
+  core.getInput.mockReturnValueOnce("abc");
+  core.getInput.mockReturnValueOnce("web");
+  fs.readFileSync.mockReturnValueOnce(
+    `
+    {
+        "name": "calver",
+        "version": "21.01.30.2",
+        "main": "dist/index.js",
+        "author": "Ankeet Maini <ankeet.maini@gmail.com>",
+        "license": "MIT",
+        "scripts": {
+            "build": "ncc build index.js --license licenses.txt"
+        },
+        "dependencies": {
+            "@actions/core": "^1.6.0",
+            "@vercel/ncc": "^0.33.1"
+        },
+        "devDependencies": {
+            "jest": "^27.4.7"
+        }
+    }
+    `
+  );
+  calver();
+  expect(fs.writeFileSync).toHaveBeenCalledWith(
+    "abc",
+    `
+    {
+        "name": "calver",
+        "version": "22.01.30.0",
+        "main": "dist/index.js",
+        "author": "Ankeet Maini <ankeet.maini@gmail.com>",
+        "license": "MIT",
+        "scripts": {
+            "build": "ncc build index.js --license licenses.txt"
+        },
+        "dependencies": {
+            "@actions/core": "^1.6.0",
+            "@vercel/ncc": "^0.33.1"
+        },
+        "devDependencies": {
+            "jest": "^27.4.7"
+        }
+    }
+    `
+  );
+});
+
+test("updates gradle file", () => {
+  core.getInput.mockReturnValueOnce("abc");
+  core.getInput.mockReturnValueOnce("android");
+  fs.readFileSync.mockReturnValueOnce(
+    `
+    import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+    plugins {
+        id("com.android.application")
+        kotlin("android")
+        kotlin("android.extensions")
+    }
+    
+    android {
+        compileSdkVersion(27)
+        defaultConfig {
+            applicationId = "org.gradle.kotlin.dsl.samples.androidstudio"
+            minSdkVersion(15)
+            targetSdkVersion(27)
+            versionCode = 1
+            versionName = "1.0"
+            testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+        }
+        buildTypes {
+            getByName("release") {
+                isMinifyEnabled = false
+                proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            }
+        }
+    }
+    
+    dependencies {
+        implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+        implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
+        implementation("com.android.support:appcompat-v7:27.1.1")
+        implementation("com.android.support.constraint:constraint-layout:1.1.0")
+        testImplementation("junit:junit:4.12")
+        androidTestImplementation("com.android.support.test:runner:1.0.2")
+        androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+    }
+      `
+  );
+  calver();
+  expect(fs.writeFileSync).toHaveBeenCalledWith(
+    "abc",
+    `
+    import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+    plugins {
+        id("com.android.application")
+        kotlin("android")
+        kotlin("android.extensions")
+    }
+    
+    android {
+        compileSdkVersion(27)
+        defaultConfig {
+            applicationId = "org.gradle.kotlin.dsl.samples.androidstudio"
+            minSdkVersion(15)
+            targetSdkVersion(27)
+            versionCode = 2
+            versionName = "22.01.30.0"
+            testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+        }
+        buildTypes {
+            getByName("release") {
+                isMinifyEnabled = false
+                proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            }
+        }
+    }
+    
+    dependencies {
+        implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+        implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
+        implementation("com.android.support:appcompat-v7:27.1.1")
+        implementation("com.android.support.constraint:constraint-layout:1.1.0")
+        testImplementation("junit:junit:4.12")
+        androidTestImplementation("com.android.support.test:runner:1.0.2")
+        androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+    }
+      `
+  );
+});
+
+test("updates gradle file same date", () => {
+  core.getInput.mockReturnValueOnce("abc");
+  core.getInput.mockReturnValueOnce("android");
+  fs.readFileSync.mockReturnValueOnce(
+    `
+      import org.jetbrains.kotlin.config.KotlinCompilerVersion
+  
+      plugins {
+          id("com.android.application")
+          kotlin("android")
+          kotlin("android.extensions")
+      }
+      
+      android {
+          compileSdkVersion(27)
+          defaultConfig {
+              applicationId = "org.gradle.kotlin.dsl.samples.androidstudio"
+              minSdkVersion(15)
+              targetSdkVersion(27)
+              versionCode = 456
+              versionName = "22.01.30.34"
+              testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+          }
+          buildTypes {
+              getByName("release") {
+                  isMinifyEnabled = false
+                  proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+              }
+          }
+      }
+      
+      dependencies {
+          implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+          implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
+          implementation("com.android.support:appcompat-v7:27.1.1")
+          implementation("com.android.support.constraint:constraint-layout:1.1.0")
+          testImplementation("junit:junit:4.12")
+          androidTestImplementation("com.android.support.test:runner:1.0.2")
+          androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+      }
+        `
+  );
+  calver();
+  expect(fs.writeFileSync).toHaveBeenCalledWith(
+    "abc",
+    `
+      import org.jetbrains.kotlin.config.KotlinCompilerVersion
+  
+      plugins {
+          id("com.android.application")
+          kotlin("android")
+          kotlin("android.extensions")
+      }
+      
+      android {
+          compileSdkVersion(27)
+          defaultConfig {
+              applicationId = "org.gradle.kotlin.dsl.samples.androidstudio"
+              minSdkVersion(15)
+              targetSdkVersion(27)
+              versionCode = 457
+              versionName = "22.01.30.35"
+              testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+          }
+          buildTypes {
+              getByName("release") {
+                  isMinifyEnabled = false
+                  proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+              }
+          }
+      }
+      
+      dependencies {
+          implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+          implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
+          implementation("com.android.support:appcompat-v7:27.1.1")
+          implementation("com.android.support.constraint:constraint-layout:1.1.0")
+          testImplementation("junit:junit:4.12")
+          androidTestImplementation("com.android.support.test:runner:1.0.2")
+          androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+      }
+        `
+  );
+});
--- index.js
+++ index.js
@@ -1,74 +1,84 @@
-const core = require('@actions/core');
-const fs = require('fs');
+const core = require("@actions/core");
+const fs = require("fs");
 
 const versionCodeRegex = new RegExp(/versionCode\s*=\s*(\d*)/);
 const versionNameRegex = new RegExp(/versionName\s*=\s*"([0-9|.|a-z]*)"/);
-const calverRegex = new RegExp(/\d{2}.\d{2}.\d{2}.((\d){1})(-[a-z].*)?/);
-const packageVersion = new RegExp(/"version"\s*:\s*"[^\s]*"/);
+const calverRegex = new RegExp(/\d{2}.\d{2}.\d{2}.((\d).*)(-[a-z].*)?/);
+const packageVersion = new RegExp(/"version"\s*:\s*"([^\s]*)"/);
 
-const pad = n => {
-    n = n + '';
-    if (n.length === 2) return n;
-    if (n.length === 1) return '0' + n;
-}
+const pad = (n) => {
+  n = n + "";
+  if (n.length === 2) return n;
+  if (n.length === 1) return "0" + n;
+};
 
 const isCalver = (version) => {
-    const date = new Date();
-    const newVersion = `${date.getFullYear() % 100}.${pad(date.getMonth() + 1)}.${pad(date.getDate())}`;
-    
-    let fullVersion;
-    
-    const result = calverRegex.exec(version);
-    if (!result) return newVersion + '.0';
+  const date = new Date();
+  const newVersion = `${date.getFullYear() % 100}.${pad(
+    date.getMonth() + 1
+  )}.${pad(date.getDate())}`;
 
-    const prev = result[0];
-    if (prev.slice(0, 8) === newVersion) {
-        fullVersion = `${newVersion}.${Number(result[1]) + 1}`
-    } else {
-        fullVersion = `${newVersion}.0`
-    }
+  let fullVersion;
 
-    return fullVersion;
-}
+  const result = calverRegex.exec(version);
+  if (!result) return newVersion + ".0";
 
-try {
-    const filePath = core.getInput('path');
-    const platform = core.getInput('platform');
+  const prev = result[0];
+
+  if (prev.slice(0, 8) === newVersion) {
+    fullVersion = `${newVersion}.${Number(result[1]) + 1}`;
+  } else {
+    fullVersion = `${newVersion}.0`;
+  }
+
+  return fullVersion;
+};
+
+const calver = () => {
+  try {
+    const filePath = core.getInput("path");
+    const platform = core.getInput("platform");
+
+    if (!filePath && !platform) return;
+
     const fileContents = fs.readFileSync(filePath).toString();
 
-    if (platform === 'android') {
-        
-        const [_, versionCode] = versionCodeRegex.exec(fileContents);
-        const newVersion = Number(versionCode) + 1;
+    if (platform === "android") {
+      const [_, versionCode] = versionCodeRegex.exec(fileContents);
+      const newVersion = Number(versionCode) + 1;
 
-        const [__, versionName] = versionNameRegex.exec(fileContents);
-        
-        const fullVersion = isCalver(versionName);
-        console.log({fullVersion});
-        fileContents.replace(versionCodeRegex, (main, old) => {
-            console.log(main, old);
-            const changed = main.replace(old, newVersion);
-            return changed;
-        });
+      const [__, versionName] = versionNameRegex.exec(fileContents);
 
-        fileContents.replace(versionNameRegex, (main, old) => {
-            const changed = main.replace(old, fullVersion);
-            return changed;
-        });
-        fs.writeFileSync(filePath, fileContents);
-        console.log({fileContents})
-    } else if (platform === 'web') {
+      const fullVersion = isCalver(versionName);
 
-        const packageJson = JSON.parse(fileContents);
-        const fullVersion = isCalver(packageJson.version);
-        console.log({fullVersion});
-        fileContents.replace(packageVersion, (main, old) => main.replace(old, fullVersion));
-        fs.writeFileSync(filePath, fileContents);
-        console.log({fileContents})
+      const versionUpdated = fileContents.replace(
+        versionCodeRegex,
+        (main, old) => main.replace(old, newVersion)
+      );
+
+      const versionNameUpdated = versionUpdated.replace(
+        versionNameRegex,
+        (main, old) => main.replace(old, fullVersion)
+      );
+      fs.writeFileSync(filePath, versionNameUpdated);
+    } else if (platform === "web") {
+      const packageJson = JSON.parse(fileContents);
+      const fullVersion = isCalver(packageJson.version);
+
+      const newContent = fileContents.replace(packageVersion, (main, old) =>
+        main.replace(old, fullVersion)
+      );
+
+      fs.writeFileSync(filePath, newContent);
     } else {
-        core.setFailed('Only `android` and `web` supported right now.');
+      core.setFailed("Only `android` and `web` supported right now.");
     }
-
   } catch (error) {
+    console.log(error);
     core.setFailed(error.message);
   }
+};
+
+calver();
+
+module.exports = calver;
