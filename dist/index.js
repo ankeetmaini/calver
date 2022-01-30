95182ffb2312096e718717578f4674936ad74ca8
Ankeet Maini <ankeet.maini@hotstar.com>
2022-01-30 18:41:40 +0530

    Add built file

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,97 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 932:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const core = __nccwpck_require__(186);
+const fs = __nccwpck_require__(147);
+
+const versionCodeRegex = new RegExp(/versionCode\s*=\s*(\d*)/);
+const versionNameRegex = new RegExp(/versionName\s*=\s*"([0-9|.|a-z]*)"/);
+const calverRegex = new RegExp(/\d{2}.\d{2}.\d{2}.((\d).*)(-[a-z].*)?/);
+const packageVersion = new RegExp(/"version"\s*:\s*"([^\s]*)"/);
+
+const pad = (n) => {
+  n = n + "";
+  if (n.length === 2) return n;
+  if (n.length === 1) return "0" + n;
+};
+
+const isCalver = (version) => {
+  const date = new Date();
+  const newVersion = `${date.getFullYear() % 100}.${pad(
+    date.getMonth() + 1
+  )}.${pad(date.getDate())}`;
+
+  let fullVersion;
+
+  const result = calverRegex.exec(version);
+  if (!result) return newVersion + ".0";
+
+  const prev = result[0];
+
+  if (prev.slice(0, 8) === newVersion) {
+    fullVersion = `${newVersion}.${Number(result[1]) + 1}`;
+  } else {
+    fullVersion = `${newVersion}.0`;
+  }
+
+  return fullVersion;
+};
+
+const calver = () => {
+  try {
+    const filePath = core.getInput("path");
+    const platform = core.getInput("platform");
+
+    if (!filePath && !platform) return;
+
+    const fileContents = fs.readFileSync(filePath).toString();
+
+    if (platform === "android") {
+      const [_, versionCode] = versionCodeRegex.exec(fileContents);
+      const newVersion = Number(versionCode) + 1;
+
+      const [__, versionName] = versionNameRegex.exec(fileContents);
+
+      const fullVersion = isCalver(versionName);
+
+      const versionUpdated = fileContents.replace(
+        versionCodeRegex,
+        (main, old) => main.replace(old, newVersion)
+      );
+
+      const versionNameUpdated = versionUpdated.replace(
+        versionNameRegex,
+        (main, old) => main.replace(old, fullVersion)
+      );
+      fs.writeFileSync(filePath, versionNameUpdated);
+    } else if (platform === "web") {
+      const packageJson = JSON.parse(fileContents);
+      const fullVersion = isCalver(packageJson.version);
+
+      const newContent = fileContents.replace(packageVersion, (main, old) =>
+        main.replace(old, fullVersion)
+      );
+
+      fs.writeFileSync(filePath, newContent);
+    } else {
+      core.setFailed("Only `android` and `web` supported right now.");
+    }
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
+  }
+};
+
+calver();
+
+module.exports = calver;
+
+
+/***/ }),
+
 /***/ 351:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -1673,85 +1764,12 @@ module.exports = require("util");
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-const core = __nccwpck_require__(186);
-const fs = __nccwpck_require__(147);
-
-const versionCodeRegex = new RegExp(/versionCode\s*=\s*(\d*)/);
-const versionNameRegex = new RegExp(/versionName\s*=\s*"([0-9|.|a-z]*)"/);
-const calverRegex = new RegExp(/\d{2}.\d{2}.\d{2}.((\d){1})(-[a-z].*)?/);
-const packageVersion = new RegExp(/"version"\s*:\s*"[^\s]*"/);
-
-const pad = n => {
-    n = n + '';
-    if (n.length === 2) return n;
-    if (n.length === 1) return '0' + n;
-}
-
-const isCalver = (version) => {
-    const date = new Date();
-    const newVersion = `${date.getFullYear() % 100}.${pad(date.getMonth() + 1)}.${pad(date.getDate())}`;
-    
-    let fullVersion;
-    
-    const result = calverRegex.exec(version);
-    if (!result) return newVersion + '.0';
-
-    const prev = result[0];
-    if (prev.slice(0, 8) === newVersion) {
-        fullVersion = `${newVersion}.${Number(result[1]) + 1}`
-    } else {
-        fullVersion = `${newVersion}.0`
-    }
-
-    return fullVersion;
-}
-
-try {
-    const filePath = core.getInput('path');
-    const platform = core.getInput('platform');
-    const fileContents = fs.readFileSync(filePath).toString();
-
-    if (platform === 'android') {
-        
-        const [_, versionCode] = versionCodeRegex.exec(fileContents);
-        const newVersion = Number(versionCode) + 1;
-
-        const [__, versionName] = versionNameRegex.exec(fileContents);
-        
-        const fullVersion = isCalver(versionName);
-        console.log({fullVersion});
-        fileContents.replace(versionCodeRegex, (main, old) => {
-            console.log(main, old);
-            const changed = main.replace(old, newVersion);
-            return changed;
-        });
-
-        fileContents.replace(versionNameRegex, (main, old) => {
-            const changed = main.replace(old, fullVersion);
-            return changed;
-        });
-        fs.writeFileSync(filePath, fileContents);
-        console.log({fileContents})
-    } else if (platform === 'web') {
-
-        const packageJson = JSON.parse(fileContents);
-        const fullVersion = isCalver(packageJson.version);
-        console.log({fullVersion});
-        fileContents.replace(packageVersion, (main, old) => main.replace(old, fullVersion));
-        fs.writeFileSync(filePath, fileContents);
-        console.log({fileContents})
-    } else {
-        core.setFailed('Only `android` and `web` supported right now.');
-    }
-
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-})();
-
-module.exports = __webpack_exports__;
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(932);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
 /******/ })()
 ;
