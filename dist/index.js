7503dc65607b47dae5d9717a74762ff0ed873ba2
Ankeet Maini <ankeet.maini@hotstar.com>
2022-01-30 20:31:06 +0530

    package

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,100 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 932:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const core = __nccwpck_require__(186);
+const fs = __nccwpck_require__(747);
+
+const versionCodeRegex = new RegExp(/versionCode\s*=\s*(\d*)/);
+const versionNameRegex = new RegExp(/versionName\s*=\s*"([0-9|.|a-z]*)"/);
+const calverRegex = new RegExp(/\d{2}.\d{2}.\d{2}.((\d).*)(-[a-z].*)?/);
+const packageVersion = new RegExp(/"version"\s*:\s*"([^\s]*)"/);
+
+const pad = (n) => {
+  n = n + "";
+  if (n.length === 2) return n;
+  if (n.length === 1) return "0" + n;
+};
+
+const isCalver = (version) => {
+  const date = new Date();
+  const newVersion = `${date.getFullYear() % 100}.${pad(
+    date.getMonth() + 1
+  )}.${pad(date.getDate())}`;
+
+  let fullVersion;
+
+  const result = calverRegex.exec(version);
+  if (!result) return newVersion + ".0";
+
+  const prev = result[0];
+
+  if (prev.slice(0, 8) === newVersion) {
+    fullVersion = `${newVersion}.${Number(result[1]) + 1}`;
+  } else {
+    fullVersion = `${newVersion}.0`;
+  }
+
+  return fullVersion;
+};
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const filePath = core.getInput("path");
+    const platform = core.getInput("platform");
+
+    if (!filePath && !platform) return;
+
+    const fileContents = fs.readFileSync(filePath).toString();
+
+    if (platform === "android") {
+      // eslint-disable-next-line no-unused-vars
+      const [_, versionCode] = versionCodeRegex.exec(fileContents);
+      const newVersion = Number(versionCode) + 1;
+      // eslint-disable-next-line no-unused-vars
+      const [__, versionName] = versionNameRegex.exec(fileContents);
+
+      const fullVersion = isCalver(versionName);
+
+      const versionUpdated = fileContents.replace(
+        versionCodeRegex,
+        (main, old) => main.replace(old, newVersion)
+      );
+
+      const versionNameUpdated = versionUpdated.replace(
+        versionNameRegex,
+        (main, old) => main.replace(old, fullVersion)
+      );
+      fs.writeFileSync(filePath, versionNameUpdated);
+    } else if (platform === "web") {
+      const packageJson = JSON.parse(fileContents);
+      const fullVersion = isCalver(packageJson.version);
+
+      const newContent = fileContents.replace(packageVersion, (main, old) =>
+        main.replace(old, fullVersion)
+      );
+
+      fs.writeFileSync(filePath, newContent);
+    } else {
+      core.setFailed("Only `android` and `web` supported right now.");
+    }
+  } catch (error) {
+    console.log(error.message);
+    console.log("-=======");
+    core.setFailed(error.message);
+  }
+}
+
+run();
+
+module.exports = run;
+
+
+/***/ }),
+
 /***/ 351:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -1673,98 +1767,13 @@ module.exports = require("util");
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-const core = __nccwpck_require__(186);
-
-const fs = __nccwpck_require__(747);
-
-const versionCodeRegex = new RegExp(/versionCode\s*=\s*(\d*)/);
-const versionNameRegex = new RegExp(/versionName\s*=\s*"([0-9|.|a-z]*)"/);
-const calverRegex = new RegExp(/\d{2}.\d{2}.\d{2}.((\d).*)(-[a-z].*)?/);
-const packageVersion = new RegExp(/"version"\s*:\s*"([^\s]*)"/);
-
-const pad = (n) => {
-  n = n + "";
-  if (n.length === 2) return n;
-  if (n.length === 1) return "0" + n;
-};
-
-const isCalver = (version) => {
-  const date = new Date();
-  const newVersion = `${date.getFullYear() % 100}.${pad(
-    date.getMonth() + 1
-  )}.${pad(date.getDate())}`;
-
-  let fullVersion;
-
-  const result = calverRegex.exec(version);
-  if (!result) return newVersion + ".0";
-
-  const prev = result[0];
-
-  if (prev.slice(0, 8) === newVersion) {
-    fullVersion = `${newVersion}.${Number(result[1]) + 1}`;
-  } else {
-    fullVersion = `${newVersion}.0`;
-  }
-
-  return fullVersion;
-};
-
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const filePath = core.getInput("path");
-    const platform = core.getInput("platform");
-
-    if (!filePath && !platform) return;
-
-    const fileContents = fs.readFileSync(filePath).toString();
-
-    if (platform === "android") {
-      // eslint-disable-next-line no-unused-vars
-      const [_, versionCode] = versionCodeRegex.exec(fileContents);
-      const newVersion = Number(versionCode) + 1;
-      // eslint-disable-next-line no-unused-vars
-      const [__, versionName] = versionNameRegex.exec(fileContents);
-
-      const fullVersion = isCalver(versionName);
-
-      const versionUpdated = fileContents.replace(
-        versionCodeRegex,
-        (main, old) => main.replace(old, newVersion)
-      );
-
-      const versionNameUpdated = versionUpdated.replace(
-        versionNameRegex,
-        (main, old) => main.replace(old, fullVersion)
-      );
-      fs.writeFileSync(filePath, versionNameUpdated);
-    } else if (platform === "web") {
-      const packageJson = JSON.parse(fileContents);
-      const fullVersion = isCalver(packageJson.version);
-
-      const newContent = fileContents.replace(packageVersion, (main, old) =>
-        main.replace(old, fullVersion)
-      );
-
-      fs.writeFileSync(filePath, newContent);
-    } else {
-      core.setFailed("Only `android` and `web` supported right now.");
-    }
-  } catch (error) {
-    console.log(error);
-    core.setFailed(error.message);
-  }
-}
-
-run();
-
-})();
-
-module.exports = __webpack_exports__;
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(932);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
 /******/ })()
 ;
 //# sourceMappingURL=index.js.map
--- index.test.js
+++ index.test.js
@@ -1,6 +1,3 @@
-const process = require("process");
-const cp = require("child_process");
-const path = require("path");
 const fs = require("fs");
 
 const core = require("@actions/core");
